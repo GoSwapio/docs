382e6d89a00585b73fe2bb65e35a17f4ba113cb0
Martijn Jansen <martijn.jansen92@gmail.com>
2023-08-29 14:24:22 +0200

    Initial build

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/martijnjansen/Documents/projects/docs.goswap.io/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/martijnjansen/Documents/projects/docs.goswap.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/martijnjansen/Documents/projects/docs.goswap.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/martijnjansen/Documents/projects/docs.goswap.io/src/css/custom.css'),
+];
